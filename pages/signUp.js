d7773622e805d804182fcc9ee0fc04e5bcd6403a
s22030 <s22030@std.it-college.ac.jp>
2023-12-21 12:02:54 +0900

    signup,login完成 logo入れた

---
+++ pages/signUp.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import {createUserWithEmailAndPassword, getAuth} from 'firebase/auth';
+import app from '../FirebaseConfig';
+import { useRouter } from 'next/router';
+import styles from "@/styles/login.module.css";
+
+const auth = getAuth(app)
+
+const AddUserPage = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const router = useRouter();
+
+    const handleRegister = async (e) => {
+        e.preventDefault();
+
+        try {
+            // FirebaseのcreateUserWithEmailAndPasswordメソッドを使用してユーザーを登録
+            await createUserWithEmailAndPassword(auth, email, password);
+            router.push('/login'); // 登録成功後、ログインページにリダイレクト
+            console.log("comp")
+        } catch (error) {
+            console.error('Registration error:', error);
+        }
+    };
+
+    return (
+        <div>
+            <h1 className={styles.h1}>ユーザー登録</h1>
+            <div className={styles.content}>
+                <form onSubmit={handleRegister}>
+                    <div>
+                        <p>Email:</p>
+                        <input className={styles.input1} type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                    </div>
+                    <div>
+                        <p>Password:</p>
+                        <input className={styles.input2} type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                    </div>
+                    <button type="submit" className={styles.blueButton}>登録</button>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default AddUserPage;
